9aeedfeea799cab64fdb43683d273298807fa4bf
Guest123 <Guest123@DESKTOP-G4USF7P>
2024-04-11 13:33:14 +0530

    Modifiers

--- Modifiers/src/testModifiers/Public.java
+++ Modifiers/src/testModifiers/Public.java
@@ -1,5 +1,21 @@
 package testModifiers;
 
 public class Public {
+	
+	static int a = 45;
+	
+	public static void meth()
+	{
+		System.out.println("meth");
+	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
---
+++ Modifiers/src/testModifiers/Public2.java
@@ -0,0 +1,12 @@
+package testModifiers;
+
+public class Public2 {
+	
+	public static void main(String[] args) {
+		System.out.println(Public.a);
+		Public.meth();
+	}
+	
+	
+
+}
