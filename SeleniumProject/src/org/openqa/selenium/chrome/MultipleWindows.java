2da27ccc32f76b2d38bdbdd7bc7977bf6c7030fe
Guest123 <Guest123@DESKTOP-G4USF7P>
2024-06-20 15:32:00 +0530

    Update

--- SeleniumProject/src/org/openqa/selenium/chrome/MultipleWindows.java
+++ SeleniumProject/src/org/openqa/selenium/chrome/MultipleWindows.java
@@ -14,29 +14,29 @@ public class MultipleWindows {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		
-		Thread.sleep(2000);
-		
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		
-		int abc = 1;
-		
-		String ParentWindow = driver.getWindowHandle();
-		System.out.println("Parent Window: " + ParentWindow);
-		System.out.println();
-		System.out.println("*****************************");
-		System.out.println();
-		
-		Thread.sleep(4000);
-		
-		driver.findElement(By.partialLinkText("OrangeHRM, Inc")).click();
-		driver.findElement(By.partialLinkText("OrangeHRM, Inc")).click();
-		driver.findElement(By.partialLinkText("OrangeHRM, Inc")).click();
-		
-		Set<String> AllWindows = driver.getWindowHandles();
-		
+//		WebDriver driver = new ChromeDriver();
+//		driver.manage().window().maximize();
+//		
+//		Thread.sleep(2000);
+//		
+//		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+//		
+//		int abc = 1;
+//		
+//		String ParentWindow = driver.getWindowHandle();
+//		System.out.println("Parent Window: " + ParentWindow);
+//		System.out.println();
+//		System.out.println("*****************************");
+//		System.out.println();
+//		
+//		Thread.sleep(4000);
+//		
+//		driver.findElement(By.partialLinkText("OrangeHRM, Inc")).click();
+//		driver.findElement(By.partialLinkText("OrangeHRM, Inc")).click();
+//		driver.findElement(By.partialLinkText("OrangeHRM, Inc")).click();
+//		
+//		Set<String> AllWindows = driver.getWindowHandles();
+//		
 //		for(String SingleWindow : AllWindows)
 //		{
 //			if(!(SingleWindow.equals(ParentWindow)))
@@ -48,12 +48,38 @@ public class MultipleWindows {
 //		}
 		
 		
-		ArrayList<String> ArrayWindows = new ArrayList<String>(AllWindows);
-//		ArrayWindows.get(1);
-		driver.switchTo().window(ArrayWindows.get(1));
-		driver.findElement(By.xpath("(//button[@class='btn btn-ohrm btn-free-demo'])[2]")).click();
+//		ArrayList<String> ArrayWindows = new ArrayList<String>(AllWindows);
+////		ArrayWindows.get(1);
+//		driver.switchTo().window(ArrayWindows.get(1));
+//		driver.findElement(By.xpath("(//button[@class='btn btn-ohrm btn-free-demo'])[2]")).click();
+		int win = 1;
+		WebDriver drivers = new ChromeDriver();
+		drivers.manage().window().maximize();
+		
+		drivers.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		String ParentWindow = drivers.getWindowHandle();
+		System.out.println("Parent Window:- " + ParentWindow);
+		System.out.println("*******************");
+		
+		Thread.sleep(4000);
+		
+		drivers.findElement(By.xpath("//a[text()='OrangeHRM, Inc']")).click();
+		drivers.findElement(By.xpath("//a[text()='OrangeHRM, Inc']")).click();
+		drivers.findElement(By.xpath("//a[text()='OrangeHRM, Inc']")).click();
+		
+		Set<String> AllWindows = drivers.getWindowHandles();
+//		System.out.println("All Windows:- "  + AllWindows);
+		
+		for(String SingleWindow : AllWindows)
+		{
+			System.out.println("Window " + win++ +":- " + SingleWindow);
+		}
 		
 		
+		
+		
+	
 		
 		
 		
@@ -62,5 +88,5 @@ public class MultipleWindows {
 		
 		
 	}
-
+	
 }
